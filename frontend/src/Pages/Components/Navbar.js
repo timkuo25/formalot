68ae03cbd0f5226ec08e821555363ad34c2e2039
timkuo25 <b05703100@ntu.edu.tw>
2022-03-21 09:19:48 +0800

    [tck] Create navbar #8

--- frontend/src/Pages/Components/Navbar.js
+++ frontend/src/Pages/Components/Navbar.js
@@ -1,6 +1,20 @@
 const Navbar = () => {
-    
-    return <></>
+    return(
+        <header className="header">
+            <h1 className="app-title">Formalot</h1>
+            <nav className="navbar">
+                <button className="nav-option">探索</button>
+                <button className="nav-option">製作</button>
+                <button className="nav-option user-dropdown">
+                    User
+                    <div className="user-dropdown-options">
+                        <button>管理問卷</button>
+                        <button>個人資料</button>
+                    </div>
+                </button>
+            </nav>
+        </header>
+    )
 }
 
 export { Navbar };
