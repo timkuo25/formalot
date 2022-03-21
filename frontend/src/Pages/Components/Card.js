2b2b28f91575a1a3fcd196590eb6aad7b823552d
timkuo25 <b05703100@ntu.edu.tw>
2022-03-21 23:58:30 +0800

    [tck] Create card #9

--- frontend/src/Pages/Components/Card.js
+++ frontend/src/Pages/Components/Card.js
@@ -1,6 +1,17 @@
 const Card = () => {
     
-    return <></>
+    return (
+        <div className="card">
+            <div className="prize-tag">現金 100 元 2 名</div>
+            <img className="q-image" src={process.env.PUBLIC_URL + 'dog.png'}/>
+            <h3>臺大校園安全問卷</h3>
+            <p>
+                截止時間：2022/3/14 20:00 <br/>
+                抽獎時間：2022/3/15 21:30
+            </p>
+            <button className="share-q">分享</button>
+        </div>
+    )
 }
 
 export { Card };
