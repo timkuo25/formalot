7bc41e11a02df7a17c45471de37f47b9bbc37e7f
unknown <s13527506@gmail.com>
2022-04-02 10:31:40 +0800

    [Sharon] update app for member api

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
 from flasgger import Swagger
 from Form.form import form_bp
+from Member.member import app_members
+from datetime import timedelta
 
 app = Flask(__name__)
+app.secret_key = "Your Key"
+app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(hours=2)
 
 
 # register blueprint
 app.register_blueprint(form_bp)
+app.register_blueprint(app_members)
 
 
 # flasgger
@@ -16,6 +21,10 @@ swag = Swagger(app)
 @app.route('/')
 def index():
     return "home"
+
+# @app.route('/', methods=['GET'])
+# def index():
+#         return render_template('index.html')
 
 
 # run
