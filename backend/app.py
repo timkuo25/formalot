e4e20ce50f1d0d5b38cbbe1360498aaa999eb5ec
chinghan <lucia0930232@gmail.com>
2022-05-20 20:09:31 +0800

    [ching] optimize backend

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,4 @@
 from flask import Flask, jsonify
-from flasgger import Swagger
 from Form.form import form_bp
 from Member.member import members_bp
 from Lottery.lottery import lottery_bp
@@ -8,11 +7,11 @@ from Explore.exploreform import explore_bp
 from datetime import timedelta
 from flask_cors import CORS
 from flask_jwt_extended import JWTManager, jwt_required, get_jwt_identity, create_access_token
-# pip3 install flask_apscheduler
 
 
 app = Flask(__name__)
-# scheduler = APScheduler()
+
+
 # register blueprint
 app.register_blueprint(form_bp)
 app.register_blueprint(members_bp)
@@ -21,44 +20,28 @@ app.register_blueprint(homePage_bp)
 app.register_blueprint(explore_bp)
 
 
-# set JWT 
+# set JWT
 app.config['JWT_SECRET_KEY'] = 'this-should-be-change'
 app.config["JWT_COOKIE_SECURE"] = False
 app.config["JWT_TOKEN_LOCATION"] = ["headers"]
 app.config['CORS_HEADERS'] = 'Content-Type'
 jwt = JWTManager(app)
 
+
 # set email
-app.config['MAIL_SERVER']='smtp.gmail.com'
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
 app.config['MAIL_USERNAME'] = 'sdmg42022@gmail.com'
 app.config['MAIL_PASSWORD'] = 'sdm2022g4'
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
 
+
 # set CORS
 CORS(app)
 
 
-app.config.update(
-    DEBUG=True,
-)
-
-# flasgger
-swag = Swagger(app)
-
-@app.route('/')
-def index():
-    return "home"
-
-@app.route("/refresh", methods=["POST"])
-@jwt_required(refresh=True)
-def refresh():
-    identity = get_jwt_identity()
-    access_token = create_access_token(identity=identity, expires_delta = timedelta(minutes=120))
-    # access_token = create_access_token(identity=identity, expires_delta = timedelta(seconds=10))
-    return jsonify(access_token=access_token, message = "success")
-
+# set header after every request
 @app.after_request
 def after_request(response):
     header = response.headers
@@ -66,7 +49,17 @@ def after_request(response):
     header['Access-Control-Allow-Headers'] = '*'
     header['Access-Control-Allow-Methods'] = '*'
     header['Content-type'] = 'application/json'
+    header['Cache-control'] = 'max-age=3600'  # general content cache
+    header['Access-Control-Max-Age'] = 3600  # preflight response cache
     return response
 
-if __name__ == "__main__":
-    app.run(host='127.0.0.1', debug=True)
+
+# refresh token
+@ app.route("/refresh", methods=["POST"])
+@ jwt_required(refresh=True)
+def refresh():
+    identity = get_jwt_identity()
+    access_token = create_access_token(
+        identity=identity, expires_delta=timedelta(minutes=120))
+    # access_token = create_access_token(identity=identity, expires_delta = timedelta(seconds=10))
+    return jsonify(access_token=access_token, message="success")
