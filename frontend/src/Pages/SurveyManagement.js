54c58b6393f536d016a4ddc16481edea960e073c
TsaiPinChiang <annerosey47@outlook.com>
2022-05-21 14:39:41 +0800

    [tsaipin]fix: Finish surveymanagement.js

--- frontend/src/Pages/SurveyManagement.js
+++ frontend/src/Pages/SurveyManagement.js
@@ -1,63 +1,98 @@
 import "../css/SurveyManagement.css";
 import { useState, useEffect } from "react";
+import { Card } from './Components/Card';
 import { Navbar } from "./Components/Navbar";
-import React from "react";
-import Edit from "./Components/survey_management_comp/Edit";
-import List from "./Components/survey_management_comp/List";
 import { Footer } from "./Components/Footer";
-// import Item from "./components/Item"
-import "./Components/survey_management_comp/SurveyManagement.css";
-
-async function fetchData(setData) {
-  const res = await fetch(`http://127.0.0.1:5000/SurveyManagement`);
-  const data = await res.json();
-  console.log(data);
-  setData(data);
-}
+import React from "react";
+import ReactLoading from "react-loading";
 
 const SurveyManagement = () => {
-  const [data, setData] = useState([]);
+  const [createdData, setCreatedData] = useState(
+    {
+      "data":[],
+      "isLoading":1
+    }
+  );
+  const [repliedData, setRepliedData] = useState(
+    {
+      "data":[],
+      "isLoading":1
+    }
+  );
 
+  // 使用 useEffect Hook
   useEffect(() => {
-    fetchData(setData);
-  }, []);
-
-  let createdData = data["created"];
-  let repliedData = data["replied"];
-  console.log(createdData);
-
-  // console.log("data",data);
-
+    console.log('execute function in useEffect');
+    let abortController = new AbortController();  
+    fetchFormData();
+    return () => {  
+        abortController.abort();  
+    }  
+  }, []);  
+  const fetchFormData = () =>
+  {
+      fetch(
+          `http://127.0.0.1:5000/SurveyManagement`,
+          {
+              method: "GET",
+              headers: {
+                  'Content-Type': 'application/json',
+                  Authorization: `Bearer ${localStorage.getItem('jwt')}`  // 驗證使用者資訊
+              }
+          }
+      )
+      .then(response => response.json())
+      .then(response => {
+        console.log('allData', response)
+        console.log('createdData',response[0].created)
+        console.log('repliedData',response[0].replied)
+        setCreatedData({
+          "data":response[0].created,
+          "isLoading":0
+        })
+        setRepliedData({
+          "data":response[0].replied,
+          "isLoading":0
+        })
+      })
+      .catch(error => console.log(error))  
+  };
   return (
     <>
       <Navbar />
-      <div className="app">
-        {/* <Edit add={setData}/> */}
-
-        {/* <h1>State:</h1>
-        <h1>{JSON.stringify(data, null, 2) }</h1> */}
-        <section className="survey-bar">
-          <div className="survey-container">
-            <h2 className="manage-header">問卷管理</h2>
-            <div className="survey-card">
-              <h3 className="manage-header">&#10004; 已填寫問卷</h3>
-              <div className="card-container-management">
-                <List listData={createdData} />
-              </div>
-            </div>
-            <div className="survey-card">
-              <h3 className="manage-header">&#10004; 已發佈問卷</h3>
-              <div className="card-container-management">
-                <List listData={repliedData} />
-              </div>
-            </div>
-            <div className="survey-manage-buttons">
-              <button className="form-button"> 填答問卷</button>
-              <button className="form-button"> 製作問卷</button>
+      <section className="survey-bar">
+        <div className="survey-container">
+          <h2 className="manage-header">問卷管理</h2>
+          <div className="survey-card card-shadow">
+            <h3 className="manage-header">&#10004; 已填寫問卷</h3>
+            <div className="card-container-management ">
+              {repliedData.isLoading ==1 ? <> <div className="loading-container"> <ReactLoading type="spinningBubbles" color="#432a58" /> </div></> : 
+                  repliedData.data.map( (form) => {
+                      return (
+                          <Card info={form} />
+                      )
+                  })
+              }
             </div>
           </div>
-        </section>
-      </div>
+          <div className="survey-card card-shadow">
+            <h3 className="manage-header">&#10004; 已發佈問卷</h3>
+            <div className="card-container-management ">
+              {createdData.isLoading==1 ? <> <div className="loading-container"> <ReactLoading type="spinningBubbles" color="#432a58" /> </div></> : 
+                createdData.data.map( (form) => {
+                    return (
+                        <Card info={form} />
+                    )
+                })
+              }
+            </div>
+          </div>
+          <div className="survey-manage-buttons">
+            <button className="form-button"> 填答問卷</button>
+            <button className="form-button"> 製作問卷</button>
+          </div>
+        </div>
+      </section>
       <Footer />
     </>
   );
