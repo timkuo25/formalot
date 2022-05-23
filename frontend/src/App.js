c10a1b210017070d9fc5650f5b09b11cf7cf509c
timkuo25 <b05703100@ntu.edu.tw>
2022-05-23 17:25:45 +0800

    fix bug

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,12 +5,12 @@ import { MakeSurvey2 } from './Pages/MakeSurvey2';
 import SurveyManagement from './Pages/SurveyManagement';
 import { Register } from './Pages/Register';
 import { Instruction } from './Pages/Instruction';
-import{ Profile } from './Pages/Profile';
+import { Profile } from './Pages/Profile';
 import { Explore } from './Pages/Explore';
 import { ForgetPassword } from './Pages/ForgetPassword';
 import { EditProfile } from './Pages/EditProfile';
 import { Form } from './Pages/Form'
-import SendEmail from './Pages/SendEmail';
+import { SendEmail } from './Pages/SendEmail';
 import  { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 const App = () => {
@@ -20,7 +20,7 @@ const App = () => {
         <Route path='/' element={<Homepage/>} />
         <Route path='/form/:form_id' element={<Form/>}/>
         <Route path='/form/:form_id/:send_email' element={<Form/>}/>
-        <Route path='/sendEmail' element={<SendEmail />}/>
+        <Route path='/sendEmail' element={<SendEmail/>}/>
         {/* <Route path='/lottery/:form_id' element={<Lottery/>} /> */}
         {/* <Route path='/Fillin/:form_id' element={<Fillin/>}/> */}
         {/* <Route path='/SurveyStatistics' element={<SurveyStatistics/>}/> */}
