c4b979778f2fed64f34bb4003bd2967200f6d84b
TsaiPinChiang <annerosey47@outlook.com>
2022-05-23 12:57:21 +0800

    [tsaipin] Add sendemail page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,27 +3,27 @@ import { Homepage } from './Pages/Homepage';
 import { MakeSurvey } from './Pages/MakeSurvey';
 import { MakeSurvey2 } from './Pages/MakeSurvey2';
 import SurveyManagement from './Pages/SurveyManagement';
-import { Lottery } from './Pages/Lottery';
 import { Register } from './Pages/Register';
 import { Instruction } from './Pages/Instruction';
 import{ Profile } from './Pages/Profile';
 import { Explore } from './Pages/Explore';
 import { ForgetPassword } from './Pages/ForgetPassword';
 import { EditProfile } from './Pages/EditProfile';
-import { Fillin } from './Pages/Fill-in'
 import { Form } from './Pages/Form'
-
-
+import SendEmail from './Pages/SendEmail';
 import  { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-const App = () => {
-  // const [form_id, setFormid] = useState(0);
 
+const App = () => {
   return (
     <Router>
       <Routes>
         <Route path='/' element={<Homepage/>} />
         <Route path='/form/:form_id' element={<Form/>}/>
-        <Route path='/lottery/:form_id' element={<Lottery/>} />
+        <Route path='/form/:form_id/:send_email' element={<Form/>}/>
+        <Route path='/sendEmail' element={<SendEmail />}/>
+        {/* <Route path='/lottery/:form_id' element={<Lottery/>} /> */}
+        {/* <Route path='/Fillin/:form_id' element={<Fillin/>}/> */}
+        {/* <Route path='/SurveyStatistics' element={<SurveyStatistics/>}/> */}
         <Route path='/instruction' element={<Instruction/>}/>
         <Route path='/SurveyManagement' element={<SurveyManagement/>}/>
         <Route path='/register' element={<Register/>}/>
@@ -33,7 +33,6 @@ const App = () => {
         <Route path='/explore' element={<Explore/>}/>
         <Route path='/ForgetPassword' element={<ForgetPassword/>}/>
         <Route path='/editProfile' element={<EditProfile/>}/>
-        <Route path='/Fillin/:form_id' element={<Fillin/>}/>
       </Routes>
     </Router>
     
